e3d5cb865fd8636a1245d772ce4ef878983f419e
adesinaolagunju <adesinaolagunju1@gmail.com>
2026-06-17 18:08:21 +0100

    Lesson 5 of the aice python course

---
+++ africa_strings.py
@@ -0,0 +1,30 @@
+sentence = "Africa is rich in oil"
+
+print(sentence)
+
+print(sentence.replace("oil", "technology"))
+
+ghana = "Kwame Nkrumah led Ghana to freedom"
+print(ghana.replace("freedom", "technological independence"))
+
+name = " Kwame Nkrumah "
+print(name)
+print(name.strip())
+
+fact = "Africa will lead through technology"
+print(fact.split())
+
+
+vision = "Africa will become a global technology leader"
+print(vision)
+print("Africa" in vision)
+print("poverty" in vision)
+
+
+print("Build Africa\nWrite Code\nCreate Solutions")
+
+print("""Build Africa
+Write Code
+Create Solutions""")
+
+
--- strings.py
+++ strings.py
@@ -6,4 +6,11 @@ print(last_name)
 
 print(first_name + " " + last_name)
 
-print(f"My full name is {first_name} {last_name}") 
+print(f"My full name is {first_name} {last_name}")
+
+
+print(len(first_name))
+print(len(last_name))
+
+print(first_name.upper())
+print(first_name.lower()) 
