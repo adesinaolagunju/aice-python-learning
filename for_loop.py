ed4d4a6ce58a99a877679ff9f1b0852dcb18e9d7
adesinaolagunju <adesinaolagunju1@gmail.com>
2026-06-18 09:50:34 +0100

    Lesson 9 in AiCE Python course for beginner

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+cofounders = ["Adesina", "Goodnews", "Samuel", "Israel"]
+
+for cofounder in cofounders:
+    print(f"{cofounder} is an AiCE Community Cofounder helping build Africa through technology.") 
+
+
+
+total = 0
+
+for number in range(1, 11):
+    total += number
+
+print(f"The sum of numbers 1 to 10 is: {total}") 
---
+++ password.py
@@ -0,0 +1,7 @@
+correct_password = "python123"
+user_input = ""
+
+while user_input != correct_password:
+    user_input = input("Enter the password: ")
+
+print("Access granted! Welcome.") 
